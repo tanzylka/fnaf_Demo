844d94963e041fba012acee81dcc3059b10153e7
tanzylka <murlakafor@gmail.com>
2026-07-02 23:54:18 +0300

    Крупное обновление
    +Сцена (офис)
    
    +Управление камерой

---
+++ 2д_управление.gd
@@ -0,0 +1,35 @@
+extends Node2D
+
+var cameraState = "idle"
+var maxAngle = 56
+
+func cameraUpdate():
+	match cameraState:
+		"left":
+			if $"../Камера".rotation_degrees[1] < maxAngle:
+				$"../Камера".rotate_y(0.05)
+		"right":
+			if $"../Камера".rotation_degrees[1] > -maxAngle:
+				$"../Камера".rotate_y(-0.05)
+		"idle":
+			if int($"../Камера".rotation_degrees[1]) != 0:
+				if $"../Камера".rotation_degrees[1] > 0:
+					$"../Камера".rotate_y(-0.05)
+				elif $"../Камера".rotation_degrees[1] < 0:
+					$"../Камера".rotate_y(0.05)
+
+func _ready() -> void:
+	pass
+
+func _process(delta: float) -> void:
+	cameraUpdate()
+
+# Функции при "наведении" на кнопку
+func _on_left_mouse_entered() -> void:
+	cameraState = "left"
+	
+func _on_right_mouse_entered() -> void:
+	cameraState = "right"
+	
+func _on_center_mouse_entered() -> void:
+	cameraState = "idle"
